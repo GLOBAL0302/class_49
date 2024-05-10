98391109496ebc392a0d870c56c699d966fae9ac
Abdashimov Kubanychbek <abdashimov99@mail.ru>
2024-05-10 08:09:02 +0400

    Task_2 is ready to go

---
+++ task_2/app.ts
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", ():void=>{
+    const root = document.getElementById("#root");
+    const allSquares = document.querySelector("#allSquares");
+
+    const addSquare = (background:string):void=>{
+        const div = document.createElement("div");
+        div.classList.add("card");
+        div.style.background = background;
+        if (allSquares)allSquares.append(div);
+    }
+
+    const squareAmount = Number(prompt("How many squares would you like: "));
+    for (let i = 0; i < squareAmount; i++){
+        let br = document.createElement("br");
+        for (let j = 0; j < squareAmount; j ++){
+            if ((i + j) % 2 !==0){
+                addSquare("white");
+            }else{
+                addSquare("black");
+            }
+        }
+        if (allSquares)allSquares.append(br);
+    }
+
+})
